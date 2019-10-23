09233d5ab9cc612a42a738f18e72d0aec84bacab
bbaquir <beverlie@hancocklab.com>
2019-10-22 17:20:25 -0700

    venns for all conditions

--- HumanMonocyte_TransandProt.R
+++ HumanMonocyte_TransandProt.R
@@ -10,17 +10,23 @@ library(dplyr)
 #upload LPS.LPS v Vehicle transcriptomic list with padj<0.05 cutoff included
 #select only Genes, log2FC and padj values
 #convert ensembl ids to gene ids by comparing to biomart_table
+#only include Log2FC values >1.5 and <-1.5
+#find out how many DE genes are upregulated and downregulated
 RNAseq <- read_csv("DEGenes_LPSLPSvsVehicle_10-2_14-06-18.csv")
 RNAseq <- RNAseq %>% 
-  select(c(1, 3, 7)) 
-RNAseq <- rename (RNAseq, Gene=X1)
+  dplyr::select(c(1, 3, 7)) 
+#RNAseq <- rename (RNAseq, Gene=X1)
 biomart_table <- rename(biomart_table, Gene=ensembl_gene_id)
 
 RNAseq_genes <- inner_join(RNAseq, biomart_table, by = "Gene") 
-RNAseq_genes <- RNAseq_genes %>% dplyr::select (c(2, 3, 4))
+RNAseq_genes <- RNAseq_genes %>% filter(log2FoldChange>1.5 | log2FoldChange < -1.5) %>% dplyr::select (c(2:4))
 RNAseq_genes <- subset(RNAseq_genes, select=c(3, 1, 2))
 RNAseq_genes <- rename(RNAseq_genes, Gene=hgnc_symbol)
 RNAseq_genes <- na.omit(RNAseq_genes) 
+
+#counts of upregulated and downregulated genes from RNASeq genes
+sum(RNAseq_genes[, c("log2FoldChange")]>0)
+sum(RNAseq_genes[, c("log2FoldChange")]<0)
 
 #upload LPS.LPS v Vehicle proteomic list
 #select only pvalues <0.05
@@ -29,7 +35,11 @@ Proteomic_genes <- filter(Proteomic, pvalue_ttest1 <0.05) %>%
   dplyr::select(c(2, 6:7))
 Proteomic_genes <- na.omit(Proteomic_genes) 
 
-##############determine shared and unique DE genes btwn RNAseq and Proteomics
+#counts of upregulated and downregulated genes from Proteomic genes
+sum(Proteomic_genes[, c("LPSLPS_Log2FC")]>0)
+sum(Proteomic_genes[, c("LPSLPS_Log2FC")]<0)
+
+##############determine shared and unique DE genes btwn RNAseq and Proteomics: LPSLPS vs Vehicle
 ##############plot in venn diagram
 install.packages("sqldf")
 library(sqldf)
@@ -62,5 +72,133 @@ ggsave(LPSLPSvVehicleVenn,
 
 
 
+##upload LPS v Vehicle transcriptomic list with padj<0.05 cutoff included
+##select only Genes, log2FC and padj values
+##convert ensembl ids to gene ids by comparing to biomart_table
+##find out how many DE genes are upregulated and downregulated
+RNAseq <- read_csv("DEGenes_LPSvsVehicle_10-2_14-06-18.csv")
+RNAseq <- RNAseq %>% 
+  dplyr::select(c(1, 3, 7)) 
+RNAseq <- rename (RNAseq, Gene=X1)
+biomart_table <- rename(biomart_table, Gene=ensembl_gene_id)
+
+RNAseq_genes <- inner_join(RNAseq, biomart_table, by = "Gene") 
+RNAseq_genes <- RNAseq_genes %>% dplyr::select (c(2:4))
+RNAseq_genes <- subset(RNAseq_genes, select=c(3, 1, 2))
+RNAseq_genes <- rename(RNAseq_genes, Gene=hgnc_symbol)
+RNAseq_genes <- na.omit(RNAseq_genes) 
+
+#counts of upregulated and downregulated genes from RNASeq genes
+sum(RNAseq_genes[, c("log2FoldChange")]>0)
+sum(RNAseq_genes[, c("log2FoldChange")]<0)
+
+#upload LPS.LPS v Vehicle proteomic list
+#select only pvalues <0.05
+Proteomic <- read_csv("DEProteins_LPSvsVehicle.csv")
+Proteomic_genes <- filter(Proteomic, pvalue_ttest1 <0.05) %>%
+  dplyr::select(c(2, 6:7))
+Proteomic_genes <- na.omit(Proteomic_genes) 
+
+#counts of upregulated and downregulated genes from Proteomic genes
+sum(Proteomic_genes[, c("LPS_Log2FC")]>0)
+sum(Proteomic_genes[, c("LPS_Log2FC")]<0)
+
+##############determine shared and unique DE genes btwn RNAseq and Proteomics: LPSLPS vs Vehicle
+##############plot in venn diagram
+#install.packages("sqldf")
+library(sqldf)
+
+#combine and generate new dataframe with both RNAseq and Proteomic lists
+RNAseqINProteomic <- sqldf('SELECT * FROM RNAseq_genes INNER JOIN Proteomic_genes ON RNAseq_genes.Gene = Proteomic_genes.Gene')
+
+#check if the number of genes are equal to the new dataframe
+length(which(Proteomic_genes$Gene %in% RNAseq_genes$Gene))
+
+#find unique genes for each omics method
+UniqueRNAseq_genes <- anti_join(RNAseq_genes, Proteomic_genes, by = "Gene" )
+UniqueProteomic_genes <- anti_join(Proteomic_genes, RNAseq_genes, by = "Gene")
+
+#find gene count of shared genes btwn omics methods
+SharedGenes <- merge(RNAseq_genes, Proteomic_genes, by= "Gene", all = FALSE)
+
+#add LPSLPSvVehicle Unique DE gene lists btwn omics methods
+LPSvVehicleVenn <- draw.pairwise.venn(area1 = 3949,area2 = 430,cross.area = 169,category = c("RNA-Seq", "Proteomics"))
+
+##venn.diagram(
+# x = list(RNAseq_genes, Proteomic_genes),
+#category.names = c("RNA-Seq" , "Proteomics"), 
+#filename = 'LPSLPSvsVehicleOmics_Venn.png',output=TRUE)
+##warning message: Length of logical index must be 1 or 2723, not 0 
+#save svg and manipulate on inkscape
+
+ggsave(LPSvVehicleVenn, 
+       file = "LPSvsVehicle_omics_Vennplot.svg", device = "svg")
+
+
   
+
+
+
+
+###upload LPSLPS v LPS transcriptomic list with padj<0.05 cutoff included
+###select only Genes, log2FC and padj values
+###convert ensembl ids to gene ids by comparing to biomart_table
+###find out how many DE genes are upregulated and downregulated
+RNAseq <- read_csv("DEGenes_LPSLPSvsLPS_strict_10-2_14-06-18.csv")
+RNAseq <- RNAseq %>% 
+  dplyr::select(c(1, 3, 7)) 
+RNAseq <- rename (RNAseq, Gene=X1)
+biomart_table <- rename(biomart_table, Gene=ensembl_gene_id)
+
+RNAseq_genes <- inner_join(RNAseq, biomart_table, by = "Gene") 
+RNAseq_genes <- RNAseq_genes %>% dplyr::select (c(2:4))
+RNAseq_genes <- subset(RNAseq_genes, select=c(3, 1, 2))
+RNAseq_genes <- rename(RNAseq_genes, Gene=hgnc_symbol)
+RNAseq_genes <- na.omit(RNAseq_genes) 
+
+#counts of upregulated and downregulated genes from RNASeq genes
+sum(RNAseq_genes[, c("log2FoldChange")]>0)
+sum(RNAseq_genes[, c("log2FoldChange")]<0)
+
+#upload LPS.LPS v Vehicle proteomic list
+#select only pvalues <0.05
+Proteomic <- read_csv("DEProteins_LPSLPSvsLPS.csv")
+Proteomic_genes <- filter(Proteomic, pvalue_ttest1 <0.05) %>%
+  dplyr::select(c(2, 14:15))
+Proteomic_genes <- na.omit(Proteomic_genes) 
+
+#counts of upregulated and downregulated genes from Proteomic genes
+sum(Proteomic_genes[, c("Log2FC")]>0)
+sum(Proteomic_genes[, c("Log2FC")]<0)
+
+##############determine shared and unique DE genes btwn RNAseq and Proteomics: LPSLPS vs Vehicle
+##############plot in venn diagram
+#install.packages("sqldf")
+library(sqldf)
+
+#combine and generate new dataframe with both RNAseq and Proteomic lists
+RNAseqINProteomic <- sqldf('SELECT * FROM RNAseq_genes INNER JOIN Proteomic_genes ON RNAseq_genes.Gene = Proteomic_genes.Gene')
+
+#check if the number of genes are equal to the new dataframe
+length(which(Proteomic_genes$Gene %in% RNAseq_genes$Gene))
+
+#find unique genes for each omics method
+UniqueRNAseq_genes <- anti_join(RNAseq_genes, Proteomic_genes, by = "Gene" )
+UniqueProteomic_genes <- anti_join(Proteomic_genes, RNAseq_genes, by = "Gene")
+
+#find gene count of shared genes btwn omics methods
+SharedGenes <- merge(RNAseq_genes, Proteomic_genes, by= "Gene", all = FALSE)
+
+#add LPSLPSvLPS Unique DE gene lists btwn omics methods
+LPSLPSvLPSVenn <- draw.pairwise.venn(area1 = 2220,area2 = 3585,cross.area = 385,category = c("RNA-Seq", "Proteomics"))
+
+##venn.diagram(
+# x = list(RNAseq_genes, Proteomic_genes),
+#category.names = c("RNA-Seq" , "Proteomics"), 
+#filename = 'LPSLPSvsVehicleOmics_Venn.png',output=TRUE)
+##warning message: Length of logical index must be 1 or 2723, not 0 
+#save svg and manipulate on inkscape
+
+ggsave(LPSLPSvLPSVenn, 
+       file = "LPSLPSvsLPS_omics_Vennplot.svg", device = "svg")
   #paired ttest: t.test(x, y, paired = TRUE, alternative = "two.sided")
